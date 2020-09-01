64a6c902ddb91a0206f3d8958d9a094c03e44de3
Ringolol <createsite108@gmail.com>
2020-09-01 05:28:34 +0300

    fix missing files

--- editor/views.py
+++ editor/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 
-from .lib.editor_func import editor_main
+from .services.editor_func import editor_main
 
 
 @login_required
