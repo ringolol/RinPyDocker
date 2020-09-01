59cfb325c05a6374da43002371b3b7ae54c68293
Ringolol <createsite108@gmail.com>
2020-09-02 02:03:26 +0300

    expand docker and add prod ver #2

---
+++ RinPy/editor/services/djex.py
@@ -0,0 +1,30 @@
+from .parser import ExpressionEvaluator
+import io
+from contextlib import redirect_stdout
+import asyncio
+
+# use docker instead of this stone-age technology!
+
+async def parse_code(code_str):
+    loop = asyncio.get_running_loop()
+    parser = ExpressionEvaluator()
+    out = await loop.run_in_executor(None, parser.parse, code_str)
+    return out
+
+async def run_code(code_str, timeout):
+    f = io.StringIO()
+    with redirect_stdout(f):
+        try:
+            await asyncio.wait_for(parse_code(code_str), timeout=timeout)
+        except asyncio.TimeoutError:
+            print(f'Program was running for more than {timeout} '+
+                    'sec and was terminated!')
+    return f.getvalue()
+
+def djex(request, code_str, file_path='', timeout=30):
+    '''exec code_str and return print output or exeption text'''
+    try:
+        out = asyncio.run(run_code(code_str, timeout))
+    except Exception as e:
+        out = str(e)
+    return out
