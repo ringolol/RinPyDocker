4b8ed16cd3b1e3223a0e6175423f77ef8af27304
Ringolol <createsite108@gmail.com>
2020-09-03 14:37:47 +0300

    add static root for nginx

--- RinPy/RinPy/settings.py
+++ RinPy/RinPy/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # serve static files for nginx
 STATIC_URL = os.environ.get('STATIC_LOC', '/static/')
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
