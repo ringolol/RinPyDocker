1d2c994bc8ab28ab624fa6da50f58f3c39fa68f7
Ringolol <createsite108@gmail.com>
2020-09-04 18:06:14 +0300

    refactor-sim

--- RinPy/editor/services/test_parser.py
+++ RinPy/editor/services/test_parser.py
@@ -96,3 +96,73 @@ plot(y, dy)
 p = ExpressionEvaluator()
 p.parse(code6)
 p.memory_dump()
+
+
+# test
+arithmetic = (2, 1)  # calced only once
+test_set = [
+    # ('', 0, arithmetic),
+    ('2*(1-2)', -2, arithmetic),
+    ('1+1*3*(5-2*(1-2))', 22, arithmetic),
+    ('x=2', 2, arithmetic),
+    ('x+1', 3, arithmetic),
+    ('y+5', Exception('exception'), arithmetic),
+    ('def k(y) { x + y }', None, arithmetic),
+    ('k(3)', 5, arithmetic),
+    ('3+4*(z=3)', Exception('exception'), arithmetic),
+    ('z', Exception('exception'), arithmetic),
+    ('def f() { 2 + 3 * 2 }', None, arithmetic),
+    ('def avg(x, y) { (x + y) / 2 }', None, arithmetic),
+    ('avg(5+3, 2*3)', 7, arithmetic),
+    ('avg(5+(3+5)*3*2+((((1)))), 2*3)', 30, arithmetic),
+    ('f()', 8, arithmetic),
+    ('avg(f(), avg(avg(2, 4), 3))', 5.5, arithmetic),
+    ('def sum(x, y, z) { x + y + z }', None, arithmetic),
+    (
+        'sum(f(), avg(sum(1,1,1),3), sum(f(), f(), avg(8, f())))', 
+        35, 
+        arithmetic
+    ),
+    ('1/0', Exception('exception'), arithmetic),
+    (
+        'x = 1; y = integ(); e = x - y; e @ y; t = time(); calc(0.02, 10); plot(t, y)', 
+        None, 
+        (0.01, 5)
+    ),
+    ('plot(t, y)', None, (0.01, 5)),
+    ('avg', None, arithmetic),
+    ('x', None, arithmetic),
+    ('arr = [1, 2, avg, x]', None, arithmetic),
+    ('arr[0]', 1, arithmetic),
+    ('arr[2](2,3)', 2.5, arithmetic),
+    ('arr[3]', 1, arithmetic),
+    ('arr2 = [14, f(), f, 88]', None, arithmetic),
+    ('arr2[0]', None, arithmetic),
+    ('arr2[1]', None, arithmetic),
+    ('arr2[2]', None, arithmetic),
+    ('arr2[2]()', None, arithmetic),
+    ('arr2[3]', None, arithmetic),
+]
+
+parser = ExpressionEvaluator()
+
+for case, real_ans, sim_pars in test_set:
+    try:
+        end_block = parser.parse(case)
+        if isinstance(end_block, Block):
+            parser.sim.calc(*sim_pars)
+            solution = end_block.outputs[0].val
+        else:
+            solution = None
+    except Exception as e:
+        solution = e
+    are_exceptions = (isinstance(real_ans, Exception) 
+            and isinstance(solution, Exception))
+    if real_ans == None or solution == real_ans or are_exceptions:
+        print(f'CORRECT -- "{case}" => "{solution}"')
+    else:
+        print(f'INCORRECT -- "{case}" => GOT "{solution}"'+
+                f' BUT INSPECTED "{real_ans}"')
+        res = 'INCORRECT'
+
+parser.memory_dump()
--- RinPy/editor/services/test_simulator.py
+++ RinPy/editor/services/test_simulator.py
@@ -13,3 +13,80 @@ class TestSimulator(unittest.TestCase):
         memory['x'] = memory['c'] - memory['c2']
         sim.calc(2, 1)
         print(sim)
+
+
+# static system
+sim = Sim()
+memory = {}
+memory['c'] = sim.create('num', [2])
+memory['z'] = memory['c'] @ sim.create('num', [3])
+memory['c2'] = sim.create('num', [3])
+memory['y'] = memory['c'] + memory['c2']
+memory['b'] = -memory['c2']
+memory['x'] = memory['c'] - memory['c2']
+sim.calc(2, 1)
+print(sim)
+
+# dynamic system #1
+sim = Sim()
+memory = {}
+memory['x'] = sim.create('num', [1])
+memory['y'] = sim.create('integ', [])
+memory['e'] = memory['x'] - memory['y']
+memory['e'] @ memory['y']
+sim.calc(0.005, 20)
+fig = tpl.figure()
+fig.plot(sim.t_hist, memory['y'].outputs[0].hist, ylim=[0, 1.1])
+fig.show()
+
+# dynamic system #2
+sim = Sim()
+memory = {}
+memory['x'] = sim.create('num', [1])
+memory['dy'] = sim.create('integ')
+memory['y'] = memory['dy'] @ sim.create('integ')
+memory['kdy'] = memory['dy'] @ sim.create('num', [0.3])
+memory['s']  = memory['kdy'] + memory['y']
+memory['e'] = memory['x'] - memory['s']
+(memory['e'] * sim.create('num', [1])) @ memory['dy']
+sim.calc(0.001, 20)
+fig = tpl.figure()
+fig.plot(sim.t_hist, memory['y'].outputs[0].hist)
+fig.show()
+fig2 = tpl.figure()
+fig2.plot(memory['y'].outputs[0].hist, memory['dy'].outputs[0].hist)
+fig2.show()
+
+# static #2
+# 2*(1-2)
+sim = Sim()
+memory = {}
+memory['x'] = sim.create('num', [1]) - sim.create('num', [2])
+memory['y'] = sim.create('num', [2])*memory['x']
+sim.calc(2, 1)
+print(memory['x'], memory['y'])
+
+# static #3
+sim = Sim()
+memory = {}
+memory['y'] = sim.create('num', [14]) / sim.create('num', [2])
+sim.calc(2, 1)
+print(memory['y'])
+
+# dynamic #3
+sim = Sim()
+memory = {}
+memory['y'] = sim.create('integ')
+sim.calc(0.01, 5)
+fig = tpl.figure()
+fig.plot(sim.t_hist, memory['y'].outputs[0].hist)
+fig.show()
+
+# logic #1
+sim = Sim()
+memory = {}
+memory['x'] = sim.create('num', [15])
+memory['y'] = sim.create('num', [15])
+memory['z'] = sim.create('num', [14])
+sim.calc(2, 1)
+print(memory['x'] > memory['y'], memory['x'] == memory['y'], memory['x'] > memory['z'])
